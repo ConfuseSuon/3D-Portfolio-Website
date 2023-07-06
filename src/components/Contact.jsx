7011c8bf48dcf720c5c4b088359007a09254eb96
ConfuseSuon <sonupun500@gmail.com>
2023-07-06 16:58:13 +0545

    create: 3D portfolio website added

---
+++ src/components/Contact.jsx
@@ -0,0 +1,124 @@
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { styles } from "../style";
+import { EarthCanvas, StarsCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await emailjs.send(
+        "service_m1171wk",
+        "template_fsu7n7j",
+        {
+          from_name: form.name,
+          to_name: "Suon",
+          from_email: form.email,
+          to_email: "sonupun500@gmail.com",
+          message: form.message,
+        },
+        "_w-WHk0JfRliHqxs_"
+      );
+
+      alert("Thank you , I will get back you to as soon possible");
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      alert("Message not send");
+    }
+
+    setLoading(false);
+    setForm({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={`${styles.sectionSubText}`}> Get in touch</p>
+        <h3 className={`${styles.sectionHeadText}`}> Contact</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="what's your good name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="what's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="what do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+        {/* <StarsCanvas /> */}
+      </motion.div>
+    </div>
+  );
+};
+
+const contactCard = SectionWrapper(Contact, "contact");
+export default contactCard;
